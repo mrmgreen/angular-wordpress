a177f8147f76cb5ac8196e833eaca4cda70f4a8c
mrmgreen <m.r.green@hotmail.co.uk>
2015-10-28 15:28:43 +0000

    homepage = homepage page, posts == posts

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -5,10 +5,29 @@ angular.module('myApp.view2', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'postsCtrl',
+    controllerAs: 'posts'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.factory('posts', ['$http', function($http) {
+
+  return {
+    message: 'eat me today!',
+
+    posts: function() {
+      return $http.get('http://localhost:8888/angular/angular-wordpress/1/wordpress/wp-json/wp/v2/posts');
+    }
+
+  }
+}])
+
+.controller('postsCtrl', ['posts', function(posts) {
+
+    var self = this;
+    posts.posts().then(function(response) {
+      self.posts = response.data;
+    })
+
 
 }]);
