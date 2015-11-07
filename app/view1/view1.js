8bb4a9556d8707ef889deeac8a38f6bd737d3a63
Martin Green <m.r.green@hotmail.co.uk>
2015-11-07 07:54:35 +0000

    working on flashing cursor

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -23,23 +23,26 @@ angular.module('myApp.view1', ['ngRoute', 'ngSanitize', 'myApp.config'])
 }])
 
 .factory('terminalText', function() {
-  
-document.querySelector('.terminal').innerHTML = '<span class="blink">hello</span>' + document.querySelector('.terminal').innerHTML;
+ 
+  function blinky() { 
+    document.querySelector('.terminal').innerHTML = '<span class="blink">hello</span>' + document.querySelector('.terminal').innerHTML;
 
-var blinkText = document.querySelector('.blink');
+    var blinkText = document.querySelector('.blink');
 
-var blink = setInterval(function() {if (blinkText.style.opacity == 0 || blinkText.style.opacity == '') { blinkText.style.opacity = 1 } else {blinkText.style.opacity = 0 }}, 1000);
+    var blink = setInterval( function() { if (blinkText.style.opacity == 0 || blinkText.style.opacity == '' ) { blinkText.style.opacity = 1 } else {blinkText.style.opacity = 0 }}, 1000);
+  }
 
   return {
-    terminal: 'tet'
+    terminal: blinky
 
   }
 })
 
-.controller('View1Ctrl', ['view1Fact', function(view1Fact) {
+.controller('View1Ctrl', ['view1Fact', 'terminalText', function(view1Fact, terminalText) {
     var self = this;
     this.message = "Take the blue pill";
     this.newMessage = view1Fact.newMessage;
+    this.terminalText = terminalText.terminal;
     view1Fact.pages().then(function(response) {
       self.homepage = response.data;
     });
