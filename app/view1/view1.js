1a5f5d1f69bb6fdc5223b76e7b5eb554687f7e19
Martin Green <m.r.green@hotmail.co.uk>
2015-11-14 14:01:18 +0000

    animates through text at manually set timeouts

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -24,16 +24,75 @@ angular.module('myApp.view1', ['ngRoute', 'ngSanitize', 'myApp.config'])
 
 .factory('terminalText', function() {
  
-  function blinky() { 
-    document.querySelector('.terminal').innerHTML = '<span class="blink">hello</span>' + document.querySelector('.terminal').innerHTML;
+  function setBlinky() { 
+
+    var blinkLetters = document.querySelector('.terminal').innerHTML;
+
+    document.querySelector('.terminal').innerHTML = '<span class="terminalText1"></span><span class="blink">&#x7c;</span>';
 
     var blinkText = document.querySelector('.blink');
 
-    var blink = setInterval( function() { if (blinkText.style.opacity == 0 || blinkText.style.opacity == '' ) { blinkText.style.opacity = 1 } else {blinkText.style.opacity = 0 }}, 1000);
+    var blink = setInterval( function() { if (blinkText.style.opacity == 0 || blinkText.style.opacity == '' ) { blinkText.style.opacity = 1 } else {blinkText.style.opacity = 0 }}, 600);
+
+    /**
+     * Animates through the string.
+     */
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,1);
+    }, 3000);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,2);
+    }, 3500);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,3);
+    }, 4000);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,4);
+    }, 4500);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,5);
+    }, 5000);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,6);
+    }, 6500);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,7);
+    }, 7000);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,8);
+    }, 8000);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,9);
+    }, 8500);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,10);
+    }, 9000);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,11);
+    }, 9500);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,12);
+    }, 10000);
+
+    setTimeout(function(){ 
+      document.querySelector('.terminalText1').innerHTML = blinkLetters.substr(0,13);
+    }, 10500);
+
   }
 
   return {
-    terminal: blinky
+    terminal: setBlinky
 
   }
 })
@@ -42,7 +101,7 @@ angular.module('myApp.view1', ['ngRoute', 'ngSanitize', 'myApp.config'])
     var self = this;
     this.message = "Take the blue pill";
     this.newMessage = view1Fact.newMessage;
-    this.terminalText = terminalText.terminal;
+    this.terminalText = terminalText.terminal();
     view1Fact.pages().then(function(response) {
       self.homepage = response.data;
     });
