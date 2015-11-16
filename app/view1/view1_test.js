e13e9752a64218f876dc5e323ded06c203b3129f
mrmgreen <m.r.green@hotmail.co.uk>
2015-11-16 16:51:20 +0000

    jasmine working without error msgs

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+fdescribe('myApp.view1 module', function() {
 
   beforeEach(module('myApp.view1'));
 
--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,9 @@ module.exports = function(config){
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/components/**/*.js',
+      'app/config/*.js',
       'app/view*/**/*.js'
     ],
 
