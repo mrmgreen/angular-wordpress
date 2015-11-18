4a42d64da74f57729169d38fe9c5e635ac3c528e
mrmgreen <m.r.green@hotmail.co.uk>
2015-11-18 11:58:30 +0000

    configured httpbackend to use regex as well as constant

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -8,26 +8,39 @@ fdescribe('myApp.view1 module', function() {
   var httpBackend,
     scope,
     jsonResponse = { title: 'first page'},
+    jsonResponse1 = { title: 'second page'},
     myConfig;
 
-    beforeEach(inject(function ($rootScope, $controller, $httpBackend, $http, _myConfig_) {
-      scope = $rootScope.$new();
-      myConfig = _myConfig_;
-      httpBackend = $httpBackend;
-      $httpBackend.whenGET(myConfig.wordpressPages).respond(jsonResponse);
-      $controller('View1Ctrl as view1', {
-        $scope: scope,
-        $http: $http
-      });
-    }));
+  beforeEach(inject(function ($rootScope, $controller, $httpBackend, $http, _myConfig_) {
+    scope = $rootScope.$new();
+    myConfig = _myConfig_;
+    httpBackend = $httpBackend;
+    //$httpBackend.whenGET(myConfig.wordpressPages).respond(jsonResponse);
+    $httpBackend.whenGET(/(wp-json\/wp\/v2\/posts$)/).respond(jsonResponse);
+    $controller('View1Ctrl as view1', {
+      $scope: scope,
+      $http: $http
+    });
+  }));
 
-  it('test myConfig constant', function() {
+  it('myConfig constant myconfig.wordpressPages', function() {
     expect(myConfig.wordpressPages).toBeDefined();
   });
 
-  it('should return page titles', function() {
+  it('myConfig constant myconfig.wordpressPosts', function() {
+    expect(myConfig.wordpressPosts).toBeDefined();
+  });
+
+  it('mock httpbackend test view1 homepage promise', inject(function($httpBackend) {
+    $httpBackend.whenGET(myConfig.wordpressPages).respond(jsonResponse);
     httpBackend.flush();
     expect(scope.view1.homepage).toEqual(jasmine.objectContaining({ title: 'first page' }));
-  });
+  }));
+
+  it('using regex for httpbackend', inject(function($httpBackend) {
+    $httpBackend.whenGET(/(wp-json\/wp\/v2\/pages$)/).respond(jsonResponse1);
+    httpBackend.flush();
+    expect(scope.view1.homepage).toEqual(jasmine.objectContaining({ title: 'second page' }));
+  }));
 
 });
