d6a91e24ef1f29f834bd3111ce8e4b27a5bc8b4e
mrmgreen <m.r.green@hotmail.co.uk>
2015-11-18 10:45:39 +0000

    added myconfig angular constant to jasmine test - view1_test.js

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -3,20 +3,27 @@
 fdescribe('myApp.view1 module', function() {
 
   beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.config'));
 
   var httpBackend,
     scope,
-    jsonResponse = { title: 'first page' };
+    jsonResponse = { title: 'first page'},
+    myConfig;
 
-    beforeEach(inject(function ($rootScope, $controller, $httpBackend, $http) {
+    beforeEach(inject(function ($rootScope, $controller, $httpBackend, $http, _myConfig_) {
       scope = $rootScope.$new();
+      myConfig = _myConfig_;
       httpBackend = $httpBackend;
-      $httpBackend.whenGET('http://localhost:8888/angular/angular-wordpress/1/wordpress/wp-json/wp/v2/pages').respond(jsonResponse);
+      $httpBackend.whenGET(myConfig.wordpressPages).respond(jsonResponse);
       $controller('View1Ctrl as view1', {
         $scope: scope,
         $http: $http
       });
     }));
+
+  it('test myConfig constant', function() {
+    expect(myConfig.wordpressPages).toBeDefined();
+  });
 
   it('should return page titles', function() {
     httpBackend.flush();
