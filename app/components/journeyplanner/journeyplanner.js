88158109b4939aa08c4bd99891be2abd8f62410a
Martin Green <m.r.green@hotmail.co.uk>
2015-12-23 14:47:02 +0000

    finished configuring typeahead need to style

--- app/components/journeyplanner/journeyplanner.js
+++ app/components/journeyplanner/journeyplanner.js
@@ -21,30 +21,21 @@ angular.module('myApp.journeyplanner', ['ngRoute'])
       }
     }).then(function(response){
       return response.data.matches.map(function(item){
-        console.log('there async', item.name);
-        return item.name;
+        console.log('there async', item);
+        return item;
       });
     });
   };
   //typeahead end
 
     var self = this;
-    this.journey = journeyPlannerFact.journey;
-    this.fromStopPoint;
-    this.destinations = {
-      from: {name: '', icsId: ''},
-      to: {name: '', icsId: ''},
-    };
-    this.master = {};
+    this.destinations;
     this.promiseJourney = '';
-    this.showhidelistFrom = 'show';
-    this.showhidelistTo = 'show';
-    this.update = function(destinations) {
-      self.master = angular.copy(destinations);
-      journeyPlannerFact.setJourney(self.master);
-      journeyPlannerFact.promiseJourneyFeed().then(function(response) {
+
+    this.update = function() {
+      console.log('destinations', this.destinations);
+      journeyPlannerFact.promiseJourneyFeed(this.destinations).then(function(response) {
         self.promiseJourney = response.data;
-        console.log('journePlannerbulls', response.data);
       }, function(response) {
         console.log('error with journey', reponse.data);
       });
@@ -53,31 +44,6 @@ angular.module('myApp.journeyplanner', ['ngRoute'])
     /* directive customer name */
     this.customer = {
       name: 'Derek'
-    }
-    this.setDestinationFrom = function(destinations) {
-     console.log('response', destinations );
-     self.destinations.from = destinations;
-     console.log('self destinations', self.destinations);
-     }
-    this.setDestinationTo = function(destinations) {
-     console.log('response', destinations );
-     self.destinations.to = destinations;
-     console.log('self destinations', self.destinations);
-    }
-
-    // from input options clicked
-    this.journeyFromOptionsClick = function(input) {
-      console.log('testme now', input);
-      self.destinations.from.name = input.name;
-      self.destinations.from.icsId = input.icsId;
-      self.showhidelistFrom = 'hide';
-    }
-    // to input options clicked
-    this.journeyToOptionsClick = function(input) {
-      console.log('testme now', input);
-      self.destinations.to.name = input.name;
-      self.destinations.to.icsId = input.icsId;
-      self.showhidelistTo = 'hide';
     }
 }])
 
@@ -90,16 +56,8 @@ angular.module('myApp.journeyplanner', ['ngRoute'])
 .factory('journeyPlannerFact', ['$http', function($http) {
 
     return {
-      journey: this.journey,
-      setJourney: function (data) {
-        this.journey = data;
-        console.log('setJourney this.journey', this.journey);
-      },
-      getJourney: function () {
-        return this.journey
-      },
-      promiseJourneyFeed: function() {
-        return $http.get('https://api.tfl.gov.uk/journey/journeyresults/' + this.journey.from.icsId + '/to/' + this.journey.to.icsId);
+      promiseJourneyFeed: function(destinations) {
+        return $http.get('https://api.tfl.gov.uk/journey/journeyresults/' + destinations.from.icsId + '/to/' + destinations.to.icsId);
       }
     }
 }]);
