fdb68004e36c09e9120196041a671324ff56b14e
Martin Green <m.r.green@hotmail.co.uk>
2016-01-12 09:19:42 +0000

    reconfig to protractor

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -9,7 +9,7 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8000/app/',
+  baseUrl: 'http://localhost:8011/wordpress-angular/angular-wordpress/app/',
 
   framework: 'jasmine',
 
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,41 +2,43 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('star wars home page', function() {
 
+  it('should navigate too homepage', function() {
+    browser.get('./');
+    expect(browser.getLocationAbsUrl()).toMatch("/starwars");
 
-  it('should automatically redirect to /terminaltext when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/terminaltext");
-  });
+  })
 
-
-  describe('terminaltext', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/terminaltext');
-    });
-
-
-    it('should render terminaltext when user navigates to /terminaltext', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('posts', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/posts');
-    });
-
-
-    it('should render posts when user navigates to /posts', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
+
+  // it('should automatically redirect to /terminaltext when location hash/fragment is empty', function() {
+  //   browser.get('index.html');
+  //   expect(browser.getLocationAbsUrl()).toMatch("/terminaltext");
+  // });
+
+
+  // describe('terminaltext', function() {
+
+  //   beforeEach(function() {
+  //     browser.get('index.html#/terminaltext');
+  //   });
+
+
+  //   it('should render terminaltext when user navigates to /terminaltext', function() {
+  //     expect(element.all(by.css('[ng-view] p')).first().getText()).
+  //       toMatch(/partial for view 1/);
+  //   });
+
+
+  // describe('posts', function() {
+
+  //   beforeEach(function() {
+  //     browser.get('index.html#/posts');
+  //   });
+
+
+  //   it('should render posts when user navigates to /posts', function() {
+  //     expect(element.all(by.css('[ng-view] p')).first().getText()).
+  //       toMatch(/partial for view 2/);
+  //   });
